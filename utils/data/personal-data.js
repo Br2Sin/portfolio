5a65d0d35b170181ba40d70643236e0f1f6d46b4
Monkey.D.Bresin <monkeydbresin@email.com>
2024-09-10 16:11:14 +0900

    change title

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Daniel Bonar",
   profile: '/profile.png',
-  designation: "Blockchain",
+  designation: "Blockchain Developer",
   description: `Passionate and innovative Blockchain Developer with over 5 years of experience, specializing in Solana blockchain technology. Adept at designing and deploying smart contracts, decentralized applications (dApps), and DeFi solutions. Skilled in Rust, Solidity, and JavaScript, with a strong focus on performance optimization, security, and scalability. Proven track record in contributing to open-source projects and collaborating effectively in remote environments. Looking to leverage my expertise to help organizations develop cutting-edge blockchain solutions.`,
   email: 'monkeydbresin@gmail.com',
   address: 'Warsaw, Poland',
